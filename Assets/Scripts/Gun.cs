ad649c49f3eebe5f069414014a45933d72b156d8
Olivier3456 <olivier.larrey@gmail.com>
2023-02-03 14:41:07 +0100

    Added impact particle, gun fire particle + target (canette)

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    [SerializeField] private Animator anim;
+    [SerializeField] private Animator _anim;
     [SerializeField] private AudioSource _gunAudioSource;
+    [SerializeField] private Transform _endGunBarrel;
+    [SerializeField] private ParticleSystem _impactParticle;
+    [SerializeField] private ParticleSystem _gunFireParticle;
+    [SerializeField] private float _gunForce = 1f;
 
-
-   
-
+    private Vector3 impactPosition;
+    
     public void Fire()
-    {       
+    {
         // Shot only if the gun fire is over.
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Gun_Idle"))
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Gun_Idle"))
         {
-            anim.SetTrigger("Shot");
+            _anim.SetTrigger("Shot");
             _gunAudioSource.Play();
+
+            ParticleSystem gFParticle = Instantiate(_gunFireParticle, _endGunBarrel.position, Quaternion.identity);
+            gFParticle.transform.forward = _endGunBarrel.forward;
+            gFParticle.Play();
+            
+
+
+            
+            Ray ray = new Ray(_endGunBarrel.position, _endGunBarrel.forward); // (Le raycast part du bout du canon.)
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                impactPosition = hit.point;
+                Debug.Log(impactPosition);
+                
+                ParticleSystem sparks = Instantiate(_impactParticle, impactPosition, Quaternion.identity);
+                sparks.transform.forward = hit.normal;   // (Oriente les particules à angle droit par rapport à la surface d'impact.)
+                sparks.Play();
+
+
+
+                if (hit.transform.gameObject.CompareTag("Canette"))
+                {
+                   hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(-hit.normal * _gunForce, ForceMode.Impulse);
+                }
+            }
         }
     }
 }
