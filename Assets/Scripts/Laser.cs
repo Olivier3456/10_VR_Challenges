d1de66de93270122c4c00371b43c20243ae5e693
Olivier3456 <olivier.larrey@gmail.com>
2023-02-03 16:58:46 +0100

    Laser added

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private Transform _endGunBarrel;
+    private MeshRenderer _laserRenderer;
+
+    private Vector3 impactPosition;
+
+
+    private void Start()
+    {
+        _laserRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+
+    void Update()
+    {
+        Ray ray = new Ray(_endGunBarrel.position, _endGunBarrel.forward); // (Le raycast part du bout du canon.)
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            impactPosition = hit.point;
+            _laserRenderer.enabled = true;
+            transform.position = impactPosition;
+            transform.transform.up = hit.normal;   // (Oriente le point du laser angle droit par rapport à la surface d'impact.)
+        }
+        else
+        {
+            _laserRenderer.enabled = false;
+        }
+    }
+}
