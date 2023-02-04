57e70d152d885b34cd7ceafda338c031f9c87597
Olivier3456 <olivier.larrey@gmail.com>
2023-02-04 12:39:47 +0100

    Laser button properly set

--- Assets/Scripts/Laser_button.cs
+++ Assets/Scripts/Laser_button.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Laser_button : MonoBehaviour
 {    
     [SerializeField] private AudioSource _laserButtonAudioSource;
+    private bool _timer = true;
 
     public bool LaserIsActive { get ; private set; }
 
-
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Hands"))
+        if (other.CompareTag("Hands") && _timer)
         {
+            _timer = false;
             LaserIsActive = !LaserIsActive;
+            _laserButtonAudioSource.Play();
+            StartCoroutine(delayBeforeNextPush());
         }
+    }
+
+    IEnumerator delayBeforeNextPush()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _timer = true;
     }
 }
