b0361ead37f8c458e07c6090814314db0bec5576
Olivier3456 <olivier.larrey@gmail.com>
2023-02-08 15:12:19 +0100

    Wheel fully functional

--- Assets/Scripts/Safe/RotateAroundProjection.cs
+++ Assets/Scripts/Safe/RotateAroundProjection.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,43 +12,31 @@ public class RotateAroundProjection : MonoBehaviour
     [SerializeField] private InputActionReference _unGripWithRightHand;     // Grip action sur la même gâchette, réglée en Press > Release Only.
     private bool _isGrippedByRightHand;
 
-
     [SerializeField] private GameObject rightHand;
     [SerializeField] private GameObject leftHand;
 
-    private Vector3 rightHandLastPosition;
-    private Vector3 rightHandNewPosition;
-
-    private Vector3 leftHandLastPosition;
-    private Vector3 leftHandNewPosition;
+    private Vector3 rightHandLastPosition;    
 
     private int rightHandInColliders = 0;       // La roue a deux colliders, la main ne pourra la tourner que lorsqu'elle sera dans les deux colliders à la fois.
-    private int leftHandInColliders = 0;
-
-    [SerializeField] private float speed = 50000;
-
-    private float wheelRadius;
-    private float wheelPerimeter;
-
+    
     private AudioSource _wheelAudioSource;
     private Vector3 lastRotationClick;
     private bool alreadyClickedOnReturn;
     [SerializeField] private float _angleBetweenClicks = 5;
 
-
-
-    [SerializeField] private GameObject ObjectToProjectJustForTest;
+ //   [SerializeField] private GameObject ObjectToProjectJustForTest;
     [SerializeField] private GameObject parentObject;
+    [SerializeField] private Transform pivot;
+
     private Vector3 pointToProject;
     private Vector3 wheelNormal;
     private Vector3 planePoint;
-
-
+    private Vector3 handProjection;
 
 
 
     void Update()
-    {        
+    {
         ProjectHandPositionToWheelPlan();
     }
 
@@ -56,40 +45,36 @@ public class RotateAroundProjection : MonoBehaviour
         wheelNormal = parentObject.transform.forward;
         pointToProject = rightHand.transform.position;
         planePoint = transform.position;
-        Vector3 projection = pointToProject - (Vector3.Dot(wheelNormal, pointToProject - planePoint) / Vector3.Dot(wheelNormal, wheelNormal)) * wheelNormal;
-        ObjectToProjectJustForTest.transform.position = projection;
+        handProjection = pointToProject - (Vector3.Dot(wheelNormal, pointToProject - planePoint) / Vector3.Dot(wheelNormal, wheelNormal)) * wheelNormal;
+     //   ObjectToProjectJustForTest.transform.position = handProjection;
     }
 
     private void Start()
     {
-        wheelRadius = transform.localScale.x * 0.5f;
-        wheelPerimeter = wheelRadius * 2 * 3.14159f;
-        Debug.Log("Wheel radius: " + wheelRadius + "; wheel perimeter: " + wheelPerimeter);
-
-
         _wheelAudioSource = GetComponent<AudioSource>();
         lastRotationClick = transform.forward;
         _angleBetweenClicks /= 90;
 
+        EnableInputActions();
+    }
 
+    private void EnableInputActions()
+    {
         _gripWithRightHand.action.Enable();
         _gripWithRightHand.action.performed += GripTheWheel;
         _unGripWithRightHand.action.Enable();
         _unGripWithRightHand.action.performed += UnGripTheWheel;
     }
 
-
     private void GripTheWheel(InputAction.CallbackContext obj)
     {
         _isGrippedByRightHand = true;
-        rightHandLastPosition = rightHand.transform.position;
+        rightHandLastPosition = handProjection;
     }
     private void UnGripTheWheel(InputAction.CallbackContext obj)
     {
         _isGrippedByRightHand = false;
     }
-
-
 
 
     private void OnTriggerEnter(Collider other)     // Méthode appelée chaque fois que quelque chose entre dans un des deux colliders.
@@ -98,37 +83,27 @@ public class RotateAroundProjection : MonoBehaviour
         {
             rightHandInColliders++;
             if (rightHandInColliders == 2)
-                rightHandLastPosition = rightHand.transform.position;
+                rightHandLastPosition = handProjection;
         }
     }
 
 
     private void OnTriggerStay(Collider other)
     {
-        float distanceHandToAxe = Vector3.Distance(rightHand.transform.position, transform.position);
-
-        // La main droite peut faire bouger la roue si elle est dans les deux colliders, si le bouton du grip est pressée, et si elle est à peu près située sur le tour de la roue.
-        if (rightHandInColliders == 2 && _isGrippedByRightHand && distanceHandToAxe > wheelRadius - 0.09f && distanceHandToAxe < wheelRadius + 0.01f)
+        // La main droite peut faire bouger la roue si elle est dans les deux colliders, et si le bouton du grip est pressé.
+        if (rightHandInColliders == 2 && _isGrippedByRightHand)
         {
-            rightHandNewPosition = rightHand.transform.position;
+            Vector3 directionFromPivotToLastHandProjection = Vector3.Normalize(pivot.position - handProjection);
+            Vector3 directionFromPivotToNewHandProjection = Vector3.Normalize(pivot.position - rightHandLastPosition);
+                        
+            float angle = Vector3.SignedAngle(directionFromPivotToNewHandProjection, directionFromPivotToLastHandProjection, Vector3.forward);
 
-            // La roue va tourner autour de son propre axe, spécifié comme étant local (transform.TransformDirection) :
-            transform.RotateAround(transform.position, transform.TransformDirection(Vector3.up), TurnForce(rightHandNewPosition, rightHandLastPosition) * speed * Time.deltaTime);
+            // La roue va tourner autour de son propre axe, spécifié comme étant local (transform.TransformDirection) :            
+            transform.RotateAround(transform.position, transform.TransformDirection(Vector3.up), angle);
 
-            rightHandLastPosition = rightHandNewPosition;
+            rightHandLastPosition = handProjection;
             CheckForClick();
         }
-
-
-        //else if (leftHandInColliders == 2)
-        //{
-        //    leftHandNewPosition = leftHand.transform.position;
-        //    float turnForce = leftHandLastPosition.x - leftHandNewPosition.x;
-
-        //    transform.RotateAround(transform.position, Vector3.forward, TurnForce(leftHandNewPosition,leftHandLastPosition) * speed * Time.deltaTime);
-
-        //    leftHandLastPosition = leftHandNewPosition;
-        //}
     }
 
     private void CheckForClick()
@@ -155,45 +130,5 @@ public class RotateAroundProjection : MonoBehaviour
         {
             rightHandInColliders--;
         }
-    }
-
-
-    private float TurnForce(Vector3 newPositionOfHand, Vector3 lastPositionOfHand)
-    {
-        // Position de la main par rapport au centre de la roue :
-        float handPositionX = newPositionOfHand.x - transform.position.x;
-        float handPositionY = newPositionOfHand.y - transform.position.y;
-
-        float positiveHandPositionX = Mathf.Abs(handPositionX);   // Pour que le mouvement ne s'inverse pas quand la main passe à gauche du centre de la roue
-                                                                  // (mais on doit conserver le signe de la variable pour le calcul suivant).
-
-        float positiveHandPositionY = Mathf.Abs(handPositionY);   // Pour que le mouvement ne s'inverse pas quand la main passe en-dessous du centre de la roue.
-
-        if (handPositionY < 0) { positiveHandPositionX = -positiveHandPositionX; }  // Pour que le mouvement s'inverse quand on tourne la roue dans sa moitié basse.       
-        if (handPositionX < 0) { positiveHandPositionY = -positiveHandPositionY; }  // Pour que le mouvement s'inverse quand on tourne la roue dans sa moitié gauche.
-
-        float xFactor = positiveHandPositionX / wheelPerimeter;        // Pour que nos variables dépendent de la taille de la roue.
-        float yFactor = positiveHandPositionY / wheelPerimeter;
-
-
-        float handMovementX = lastPositionOfHand.x - newPositionOfHand.x;   // Distance parcourue par la main depuis la dernière image.
-        float handMovementY = lastPositionOfHand.y - newPositionOfHand.y;
-
-
-        float turnForce;
-        if (xFactor < 0 && yFactor > 0 || xFactor > 0 && yFactor < 0) turnForce = -(handMovementX * yFactor - handMovementY * xFactor);
-        else turnForce = handMovementX * yFactor - handMovementY * xFactor;
-
-
-
-
-        //float axeToHandDistanceFactor = 1 / (Vector3.Distance(newPositionOfHand, transform.position) / wheelRadius);  // Plus la main est proche de l'axe, plus la roue va tourner vite pour un même mouvement de main.
-        //if (axeToHandDistanceFactor < 1) axeToHandDistanceFactor = 1;
-        //else if (axeToHandDistanceFactor > 10) axeToHandDistanceFactor = 10;    // On va le limiter à 10 pour que la roue ne tourne pas trop vite quand la main est très proche de son axe.
-
-        //turnForce *= axeToHandDistanceFactor;
-        //Debug.Log("Distance de la main au centre de la roue : " + (Vector3.Distance(newPositionOfHand, transform.position) + "Divisé par le rayon de la roue : " + (Vector3.Distance(newPositionOfHand, transform.position) / wheelRadius) + " . axeToHandDistanceFactor : " + axeToHandDistanceFactor));
-
-        return turnForce;
     }
 }
