d1a63126d4722253d2d7eba7b1986da3525b2711
Olivier3456 <olivier.larrey@gmail.com>
2023-02-17 11:35:36 +0100

    Adjustment on the Knife_Raycast class for a better plant

--- Assets/Scripts/Throw_Ninja/Knife_Raycast.cs
+++ Assets/Scripts/Throw_Ninja/Knife_Raycast.cs
@@ -34,71 +34,65 @@ public class Knife_Raycast : MonoBehaviour
 
     void Start()
     {
-        lastPosition = transform.position;        
+        lastPosition = transform.position;
         actualPosition = transform.position;
-
         lastLastPosition = transform.position;
     }
 
 
     void Update()
-    {
+    {       
         actualPosition = transform.position;
-        
-
-        float distanceFromLastFrame = Vector3.Distance(actualPosition, lastPosition);
-        float lastDistanceFromLastFrame = Vector3.Distance(lastLastPosition, lastPosition);     // Un test pour voir si l'appréciation de la vitesse du couteau est plus fiable si on prend la vitesse une frame plus tôt (si jamais la collision a déjà ralenti le couteau lors du calcul).
-
-        Vector3 direction = (actualPosition - lastPosition).normalized;
-
 
         if (isPlanted == false) // Pour mieux faire, au lieu de cette cascade de if, il faudrait que chaque critère influe sur un même nombre
                                 // dont la valeur finale déterminera si le couteau se plante ou non. Par exemple, la vitesse, si elle est
                                 // très importante, doit pouvoir permettre au couteau de se planter, même si ses angles ne sont pas optimaux.
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, direction, out hit, maxDistToPlant))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistToPlant))
             {
                 if (hit.transform.gameObject.CompareTag("Plantable"))
                 {
-                    float hitDistance = Vector3.Distance(hit.point, transform.position);
-                    Debug.Log("Distance de l'émetteur du Raycast au point hit : " + hitDistance);
+                    float distanceFromLastFrame = Vector3.Distance(actualPosition, lastPosition);
+                    float lastDistanceFromLastFrame = Vector3.Distance(lastLastPosition, lastPosition);     // Un test pour voir si l'appréciation de la vitesse du couteau est plus fiable si on prend la vitesse une frame plus tôt (si jamais la collision a déjà ralenti le couteau lors du calcul).
 
-                    if (hitDistance < maxDistToPlant)
+                    Vector3 MovementDirection = (actualPosition - lastPosition).normalized;                    
+
+                    float speed = distanceFromLastFrame / Time.deltaTime;
+                    float lastSpeed = lastDistanceFromLastFrame / Time.deltaTime;
+                    
+                    float highestSpeedOfTheTree;    // Nous prendrons la plus grande de ces trois vitesses, qui varient beaucoup entre elles au même moment, j'ignore pourquoi.
+                    if (speed >= lastSpeed) highestSpeedOfTheTree = speed;
+                    else highestSpeedOfTheTree = lastSpeed;
+                    if (highestSpeedOfTheTree <= knifeRb.velocity.magnitude) highestSpeedOfTheTree = knifeRb.velocity.magnitude;
+                    speed = highestSpeedOfTheTree;
+                    Debug.Log("Vitesse du couteau (avant-dernière image) : " + lastSpeed + ". Vitesse du couteau (dernière image) : " + speed + ". Vitesse du rigidbody : " + knifeRb.velocity.magnitude + " Valeur retenue : " + speed);
+
+                    if (speed > minSpeedToPlant)
                     {
-                        float angleOfKnifeToHitNormal = Vector3.Angle(hit.normal, transform.forward);
-                        Debug.Log("Angle entre le couteau et le plan du point hit : " + angleOfKnifeToHitNormal);
+                        float angleFromKnifeToHitNormal = Vector3.Angle(hit.normal, transform.forward);
+                        Debug.Log("Angle entre le couteau et le plan du point hit : " + angleFromKnifeToHitNormal);
 
-                        if (angleOfKnifeToHitNormal > minKnifeAngleToPlant)
+                        if (angleFromKnifeToHitNormal + speed * 2 > minKnifeAngleToPlant)
                         {
-                            float angleOfKnifeMovementToHitNormal = Vector3.Angle(hit.normal, direction);
-                            Debug.Log("Angle entre la direction du mouvement du couteau et le plan du point hit : " + angleOfKnifeMovementToHitNormal);
+                            float angleFromKnifeMovementToHitNormal = Vector3.Angle(hit.normal, MovementDirection);
+                            Debug.Log("Angle entre la direction du mouvement du couteau et le plan du point hit : " + angleFromKnifeMovementToHitNormal);
 
-                            if (angleOfKnifeMovementToHitNormal > minMovementAngleToPlant)
+                            if (angleFromKnifeMovementToHitNormal + speed * 2 > minMovementAngleToPlant)
                             {
-                                float angleFromKnifeToKnifeMovement = Vector3.Angle(direction, transform.forward);
+                                float angleFromKnifeToKnifeMovement = Vector3.Angle(MovementDirection, transform.forward);
                                 Debug.Log("Angle entre le couteau et son mouvement : " + angleFromKnifeToKnifeMovement);
 
-                                if (angleFromKnifeToKnifeMovement < maxAngleBetweenKnifeAndItsMovement)
+                                if (angleFromKnifeToKnifeMovement + speed * 2 < maxAngleBetweenKnifeAndItsMovement)
                                 {
-                                    float speed = distanceFromLastFrame / Time.deltaTime;
-                                    float lastSpeed = lastDistanceFromLastFrame / Time.deltaTime;
-
-
-                                    Debug.Log("Vitesse du couteau (avant-dernière image) : " + lastSpeed);
-                                    Debug.Log("Vitesse du couteau (dernière image) : " + speed);
-
-                                    if (speed > minSpeedToPlant)
-                                    {
-                                        float halfKnifeLength = 0.24f;
-                                        Vector3 knifePlantVector = transform.forward * (halfKnifeLength - (speed * 0.01f));
-                                        knifeRb.transform.position = hit.point - knifePlantVector; // Pour que le couteau "se plante" un peu dans le mur
-                                        knifeRb.constraints = knifeRb.constraints | RigidbodyConstraints.FreezePosition;
-                                        knifeRb.constraints = knifeRb.constraints | RigidbodyConstraints.FreezeRotation;
-                                        Debug.Log("LE COUTEAU S'EST PLANTé. IL S'EST ENFONCé DE " + knifePlantVector.magnitude + " CM.");
-                                        isPlanted = true;
-                                        audioSource.Play();
-                                    }
+                                    float halfKnifeLength = 0.24f;
+                                    Vector3 knifePlantVector = transform.forward * (halfKnifeLength - (speed * 0.01f));
+                                    knifeRb.transform.position = hit.point - knifePlantVector; // Pour que le couteau "se plante" un peu dans le mur
+                                    knifeRb.constraints = knifeRb.constraints | RigidbodyConstraints.FreezePosition;
+                                    knifeRb.constraints = knifeRb.constraints | RigidbodyConstraints.FreezeRotation;
+                                    Debug.Log("LE COUTEAU S'EST PLANTé. IL S'EST ENFONCé DE " + knifePlantVector.magnitude * 100 + " CM.");
+                                    isPlanted = true;
+                                    audioSource.Play();
                                 }
                             }
                         }
@@ -111,7 +105,7 @@ public class Knife_Raycast : MonoBehaviour
 
         lastPosition = actualPosition;
     }
-        
+
 
     private IEnumerator DelayBeforeNextPlant()
     {
